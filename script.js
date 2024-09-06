313052eba69366b2f11a00b2b8f71eac5a22db4a
Guilherme532 <guilhermeeduardo152004@gmail.com>
2024-09-06 12:26:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Animação para rolar até as seções
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
